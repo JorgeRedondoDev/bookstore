6c7a8032284c2be7dcb58cf8cabb289d3aabdc1c
Jorge <jorgeRedondoDev@gmail.com>
2021-10-13 00:31:09 +0200

    ♻️ ready for deploy

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
+  apiKey: "AIzaSyCg1G1b8bHGfbK2RFI4Uc37w9eW5gzMVIo",
+  authDomain: "bookstore-7ea9f.firebaseapp.com",
+  projectId: "bookstore-7ea9f",
+  storageBucket: "bookstore-7ea9f.appspot.com",
+  messagingSenderId: "894948988713",
+  appId: "1:894948988713:web:711f80b7bc198e31e0c938",
 };
 
 // Initialize Firebase
